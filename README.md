70ceea798297f4fec252b0977729448fbc5f58b7
Brian Huisman <bhuisman@greywyvern.com>
2023-05-09 11:50:04 -0400

    Move keyboard credit list to README.md

--- README.md
+++ README.md
@@ -5,3 +5,38 @@ Add a javascript virtual keyboard to text, password, textarea, and number inputs
 Includes a "User Script" version that can be added to browsers such as Firefox to enable the keyboard on all pages.
 
 SVG icon modified from [Icon Fonts](https://www.onlinewebfonts.com/icon); licenced by CC BY 3.0.
+
+## Keyboard Credits
+- Yiddish (Yidish Lebt) keyboard layout by Simche Taub (jidysz.net)
+- Urdu Phonetic keyboard layout by Khalid Malik
+- Yiddish keyboard layout by Helmut Wollmersdorfer
+- Khmer keyboard layout by Sovann Heng (km-kh.com)
+- Dari keyboard layout by Saif Fazel
+- Kurdish keyboard layout by Ara Qadir
+- Assamese keyboard layout by Kanchan Gogoi
+- Bulgarian BDS keyboard layout by Milen Georgiev
+- Basic Japanese Hiragana/Katakana keyboard layout by Damjan
+- Ukrainian keyboard layout by Dmitry Nikitin
+- Macedonian keyboard layout by Damjan Dimitrioski
+- Pashto keyboard layout by Ahmad Wali Achakzai (qamosona.com)
+- Armenian Eastern and Western keyboard layouts by Hayastan Project (www.hayastan.co.uk)
+- Pinyin keyboard layout from a collaboration with Lou Winklemann
+- Kazakh keyboard layout by Alex Madyankin
+- Danish keyboard layout by Verner Kjærsgaard
+- Slovak keyboard layout by Daniel Lara (www.learningslovak.com)
+- Belarusian and Serbian Cyrillic keyboard layouts by Evgeniy Titov
+- Bulgarian Phonetic keyboard layout by Samuil Gospodinov
+- Swedish keyboard layout by Håkan Sandberg
+- Romanian keyboard layout by Aurel
+- Farsi (Persian) keyboard layout by Kaveh Bakhtiyari (www.bakhtiyari.com)
+- Burmese keyboard layout by Cetanapa
+- Bosnian/Croatian/Serbian Latin/Slovenian keyboard layout by Miran Zeljko
+- Hungarian keyboard layout by Antal Sall 'Hiromacu'
+- Arabic keyboard layout by Srinivas Reddy
+- Italian and Spanish (Spain) keyboard layouts by dictionarist.com
+- Lithuanian and Russian keyboard layouts by Ramunas
+- German keyboard layout by QuHno
+- French keyboard layout by Hidden Evil
+- Polish Programmers layout by moose
+- Turkish keyboard layouts by offcu
+- Dutch and US Int'l keyboard layouts by jerone
--- keyboard.user.js
+++ keyboard.user.js
@@ -8,9 +8,8 @@
 // @license      MIT; https://opensource.org/license/mit
 // ==/UserScript==
 
-/* ********************************************************************
- **********************************************************************
- * HTML Virtual Keyboard Interface User Javascript - v1.53
+/**
+ * Virtual Keyboard Interface User Javascript - v1.53
  *   Copyright (c) 2022 - GreyWyvern
  *
  * Add a script-driven keyboard interface to text fields, password
@@ -20,50 +19,7 @@
  * See https://greywyvern.com/code/javascript/keyboard/ for examples
  * and usage instructions.
  *
- * Version 1.53 - December 27, 2022
- *   - Prevent showing mobile keyboard (if possible) when Virtual
- *     Keyboard is attached
- *   - Font size is now rem unit based instead of px based so it should
- *     scale better.
- *
- *   See full changelog at:
- *     https://github.com/GreyWyvern/virtual-keyboard/changelog.txt
- *
- * Keyboard Credits
- *   - Yiddish (Yidish Lebt) keyboard layout by Simche Taub (jidysz.net)
- *   - Urdu Phonetic keyboard layout by Khalid Malik
- *   - Yiddish keyboard layout by Helmut Wollmersdorfer
- *   - Khmer keyboard layout by Sovann Heng (km-kh.com)
- *   - Dari keyboard layout by Saif Fazel
- *   - Kurdish keyboard layout by Ara Qadir
- *   - Assamese keyboard layout by Kanchan Gogoi
- *   - Bulgarian BDS keyboard layout by Milen Georgiev
- *   - Basic Japanese Hiragana/Katakana keyboard layout by Damjan
- *   - Ukrainian keyboard layout by Dmitry Nikitin
- *   - Macedonian keyboard layout by Damjan Dimitrioski
- *   - Pashto keyboard layout by Ahmad Wali Achakzai (qamosona.com)
- *   - Armenian Eastern and Western keyboard layouts by Hayastan Project (www.hayastan.co.uk)
- *   - Pinyin keyboard layout from a collaboration with Lou Winklemann
- *   - Kazakh keyboard layout by Alex Madyankin
- *   - Danish keyboard layout by Verner Kjærsgaard
- *   - Slovak keyboard layout by Daniel Lara (www.learningslovak.com)
- *   - Belarusian and Serbian Cyrillic keyboard layouts by Evgeniy Titov
- *   - Bulgarian Phonetic keyboard layout by Samuil Gospodinov
- *   - Swedish keyboard layout by Håkan Sandberg
- *   - Romanian keyboard layout by Aurel
- *   - Farsi (Persian) keyboard layout by Kaveh Bakhtiyari (www.bakhtiyari.com)
- *   - Burmese keyboard layout by Cetanapa
- *   - Bosnian/Croatian/Serbian Latin/Slovenian keyboard layout by Miran Zeljko
- *   - Hungarian keyboard layout by Antal Sall 'Hiromacu'
- *   - Arabic keyboard layout by Srinivas Reddy
- *   - Italian and Spanish (Spain) keyboard layouts by dictionarist.com
- *   - Lithuanian and Russian keyboard layouts by Ramunas
- *   - German keyboard layout by QuHno
- *   - French keyboard layout by Hidden Evil
- *   - Polish Programmers layout by moose
- *   - Turkish keyboard layouts by offcu
- *   - Dutch and US Int'l keyboard layouts by jerone
- *
+ * - https://github.com/GreyWyvern/virtual-keyboard/
  */
 
 (function() {
